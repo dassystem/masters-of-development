fdfbd50456ab05d9bef12249eeab318ecb4bb6f9
Brian Preuß <b.preuss@tarent.de>
2018-06-19 10:53:10 +0200

    Platformpunkte #14

--- block.py
+++ block.py
@@ -4,15 +4,19 @@ class Block(pygame.sprite.Sprite):
     BLOCK_WIDTH = 32
     BLOCK_HEIGHT = 32
     
-    def __init__(self, x , y, width = BLOCK_WIDTH, height = BLOCK_HEIGHT):
+    def __init__(self, level, x , y, width = BLOCK_WIDTH, height = BLOCK_HEIGHT):
         # call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
-        
+
+        self.__level = level
         self.__rect = pygame.Rect(x, y, width, height)
         self.__color = pygame.Color(255, 0, 0)
     
     def get_rect(self):
         return self.__rect
+
+    def get_level(self):
+        return self.__level
 
     def render(self, surface):
         pygame.draw.rect(surface, self.__color, self.__rect, 1)
